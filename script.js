079872abbdec409af974968d17c2178fc23444ba
Oleg <olejka.ua2008@gmail.com>
2024-12-06 22:17:56 +0200

    [1.1.3] add code

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const tabsBtns = document.querySelectorAll(".tabs__nav button");
 //console.log(tabsBtns);
 const tabsItems = document.querySelectorAll(".tabs__item");
-//console.log(tabsItems);
+// console.log(tabsItems);
 function hideTabs() {
    tabsItems.forEach(item => item.classList.add("hide"));
    tabsBtns.forEach(item => item.classList.remove("active"));
 
 }
+
+
+
+
+
+
+
+
 function showTab(index) {
    tabsItems[index].classList.remove("hide");
    tabsBtns[index].classList.add("active");
@@ -14,10 +22,9 @@ function showTab(index) {
 
 }
 
-
-
 hideTabs();
 showTab(0);
+
 
 
 tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
@@ -25,23 +32,28 @@ tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
    showTab(index);
 }))
 
-const anchors = document.querySelectorAll(".header__nav a");
-
-
-anchors.forEach(anc => {
-   anc.addEventListener("click", function (event) {
-
-      event.preventDefault();
-      const id = anc.getAttribute("href");
-      const elem = document.querySelector(id);
-
-      window.scroll({
-         top: elem.offsetTop - 80,
-         behavior: 'smooth'
-      })
 
 
 
-   });
 
-});
+
+// const anchors = document.querySelectorAll(".header__nav a");
+
+
+// anchors.forEach(anc => {
+//    anc.addEventListener("click", function (event) {
+
+//       event.preventDefault();
+//       const id = anc.getAttribute("href");
+//       const elem = document.querySelector(id);
+
+//       window.scroll({
+//          top: elem.offsetTop - 80,
+//          behavior: 'smooth'
+//       })
+
+
+
+//    });
+
+// });
